0581a3f2d1423746efdb76e7f34bede5fd4c2d92
RuiCoimbras <rui.coimbras@pt.ibm.com>
2018-10-25 22:26:25 +0100

    initial commit

---
+++ src/main/java/com/ibm/accessiblevote/ms/users/accessiblevotemsusers/AccessiblevoteMsUsersApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.accessiblevote.ms.users.accessiblevotemsusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessiblevoteMsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessiblevoteMsUsersApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ibm/accessiblevote/ms/users/accessiblevotemsusers/AccessiblevoteMsUsersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ibm.accessiblevote.ms.users.accessiblevotemsusers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccessiblevoteMsUsersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
